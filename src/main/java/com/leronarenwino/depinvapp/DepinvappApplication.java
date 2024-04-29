4e010a1cdade9d1d403a404507e1dff05f3fac99
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-04-28 21:29:29 -0500

    :tada: Initial commit!

---
+++ src/main/java/com/leronarenwino/depinvapp/DepinvappApplication.java
@@ -0,0 +1,13 @@
+package com.leronarenwino.depinvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepinvappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepinvappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leronarenwino/depinvapp/DepinvappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leronarenwino.depinvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepinvappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
